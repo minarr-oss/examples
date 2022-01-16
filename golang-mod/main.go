a4cbd6e4be4210c66980829d12d9c264578c48fe
Neo Zhuo <neowiz@icloud.com>
2022-01-16 17:21:26 +0800

    chore: use logrus in golang example

--- golang-mod/main.go
+++ golang-mod/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 
 	"github.com/zenohq/example-apps/handlers"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.Howdy)
-	fmt.Println("Yep, I'm ready~ :8080")
+	logrus.Println("Yep, I'm ready~ :8080")
 	http.ListenAndServe(":8080", nil)
 }
